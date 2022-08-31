83f3151eb48588ed9f523d7c27b370ac62699220
unknown <laura.delafuente@outlook.com.br>
2022-08-31 18:38:56 -0300

    implementando métodos http

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,13 +1,24 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.repository.PostagemRepository;
 import com.generation.blogpessoal.model.Postagem;
@@ -17,12 +28,48 @@ import com.generation.blogpessoal.model.Postagem;
 @CrossOrigin(origins = "*", allowedHeaders = "*") //indica que a Classe controladora permitirá o recebimento de requisições realizadas de fora do domínio (front end consegue acessar)
 public class PostagemController {
 		
-		@Autowired //define quais Classes serão instanciadas e em quais lugares serão Injetadas quando houver necessidade.
+		@Autowired //define quais Classes serão instanciadas e em quais lugares serão Injetadas quando houver necessidade, transefere responsabilidade para o repositório(injeção de dependencia)
 		private PostagemRepository postagemRepository;
 		
 		@GetMapping //indica que o Método getAll(), responderá a todas as requisições do tipo HTTP GET, enviadas no endereço http://localhost:8080/postagens/.
 		public ResponseEntity<List<Postagem>> getAll(){ // ResponseEntity responderá a Requisição HTTP (HTTP Request), com uma Resposta HTTP;ListPostagem será retornado um Objeto da Classe List (Collection), contendo todos os Objetos da Classe Postagem
 			return ResponseEntity.ok(postagemRepository.findAll()); //retornará todos os Objetos da Classe Postagem persistidos no Banco de dados ListPostagem
+		}
+		
+		@GetMapping("/{id}") //indica que o Método getById( Long id ), responderá a todas as requisições do tipo HTTP GET
+		public ResponseEntity<Postagem> getById(@PathVariable Long id) { //O Método getById será do tipo ResponseEntity pq ele responderá Requisições HTTP, com uma Resposta HTTP e PathVariable insere o valor enviado no endereço do endpoint, {id} e no Método getById(Long id); postagens/1
+			return postagemRepository.findById(id) //O Método retornará um Objeto da Classe Postagem persistido no Banco de dados
+					.map(resposta -> ResponseEntity.ok(resposta)) // se o Objeto da Classe Postagem for econtrado, o Método mapeia o objeto retornado, insere na resposta e retorna ok
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); //se o objeto não for encontrado será retronado como erro. build constroi a resposta
+		}
+		
+		@GetMapping("/titulo/{titulo}")
+		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) { 
+			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		}
+		
+		@PostMapping //indica que o Método post(Postagem postagem), responderá a todas as requisições do tipo HTTP POST
+		public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) { //retornará a requisição/ @valid valida o objeto postagem conforme as regras em "Postagem" (notBlank)/ @RequestBody recebe o objeto postagem e insere no parâmetro Postagem do Método post.
+			return ResponseEntity.status(HttpStatus.CREATED) //Executa o Método padrão da Interface JpaRepository e retorna o HTTP 201 se o Objeto foi persistido no Banco de dados.
+					.body(postagemRepository.save(postagem));	//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título", "Texto", CURRENT_TIMESTAMP())
+		}
+		
+		@PutMapping //indica que o Método put(Postagem postagem), responderá a todas as requisições do tipo HTTP PUT
+		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) { 
+			return postagemRepository.findById(postagem.getId())
+					.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem))) //save: se existe o objeto, ele atualiza todos os atributos. Se não existir, ele cria um novo com os dados enviados
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		}
+		
+		@ResponseStatus(HttpStatus.NO_CONTENT) //indica que o Método delete(Long id), terá uma Response Status específica (HTTP Status NO_CONTENT 🡪 204)
+		@DeleteMapping("/{id}") //indica que o Método delete( Long id ), responderá a todas as requisições do tipo HTTP DELETE
+		public void delete(@PathVariable Long id) { //id é uma Variável de Caminho (Path Variable), que receberá o id da Postagem que será Deletada.
+			Optional<Postagem> postagem = postagemRepository.findById(id); //Objeto Optional da Classe Postagem chamado postagem. Optional evita o erro "Nulo", ao invés de utilizar map.
+			if (postagem.isEmpty()) //Se o objeto postagem está vazio retorna como 404 (não encontrado)
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND); //as próximas linhas do método serão ignoradas
+			
+			postagemRepository.deleteById(id); //executa o método e retorna HTTP Status NO_CONTENT 🡪 204
 		}
 	}
 
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,9 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -8,4 +11,5 @@ import com.generation.blogpessoal.model.Postagem;
 @Repository //indica que a Interface é do tipo repositório, responsável pela interação com o Banco de dados através dos Métodos padrão.
 public interface PostagemRepository extends JpaRepository <Postagem, Long > { //2 parametros: Classe Postagem (gerou a tb_postagens) e O Long representa a Chave Primária (id) 
 
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
